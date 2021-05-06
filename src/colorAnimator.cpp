1c2ce6ed460dd6bc36f1f85a1592291c3352c4de
eheperson <barannkuzu@gmail.com>
2021-05-06 13:49:47 +0300

    color animator added

--- src/colorAnimator.cpp
+++ src/colorAnimator.cpp
@@ -23,11 +23,13 @@ int main(int argc, char* argv[]){
         //Draw Particles
         int elapsed = SDL_GetTicks();
         //double green = (1 + sin(elapsed*0.001))*128;
-        //to animate colors uncomment : 
-        unsigned char green = (unsigned char)( (1 + sin(elapsed*0.001))*128);
+        //to animate colors : 
+        unsigned char red = (unsigned char)( (1 + sin(elapsed*0.0001))*128);
+        unsigned char green = (unsigned char)( (1 + sin(elapsed*0.0002))*128);
+        unsigned char blue = (unsigned char)( (1 + sin(elapsed*0.0003))*128);
         for( int y = 0; y < Screen :: SCREEN_HEIGHT; y++){
             for( int x = 0; x < Screen :: SCREEN_WIDTH; x++){
-                screen.setPixel(x, y, 0, green, 0);
+                screen.setPixel(x, y, red, green, blue);
             }
         }
         //screen.setPixel( 400, 300, 255, 255, 255);
